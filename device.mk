79ef135882e2bc00db73763a79e437c5cbc603d6
Peter Cai <peter@typeblog.net>
2019-09-21 16:55:22 +0800

    onclite: Add libstagefright-omx & android.hardware.media.omx@1.0-service
    Change-Id: I7334f7540662f4e8198b92ec75823b448441ccb6
    Signed-off-by: bablusss <baaswanthmadhav@gmail.com>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -296,6 +296,7 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service \
     libc2dcolorconvert \
     libmm-omxcore \
     libOmxAacEnc \
@@ -307,7 +308,8 @@ PRODUCT_PACKAGES += \
     libOmxSwVencHevc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_omx.vendor
 
 # Overlays
 PRODUCT_PACKAGES += \
