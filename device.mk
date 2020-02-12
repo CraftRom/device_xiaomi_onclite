2895274387fbe70626d162fa30bb88834a0cf4f6
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    sdm845-common: Enable Freeform Multiwindow
    Change-Id: I82965ce0c285ef52bc44b0745ec7c2b0007ec66d
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: Dhruv <dhruvgera61@gmail.com>

--- device.mk
+++ device.mk
@@ -202,6 +202,11 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/gps/,$(TARGET_COPY_OUT_VENDOR)/etc)
