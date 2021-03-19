d74875419528a32ffdad1316a3b19e823585c39f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-19 09:26:02 +0100

    onclite: Explicitly add needed camera libs for IMS
    Signed-off-by: Hadad <repo-sync@outlook.com>
    Change-Id: I862566ec861e3ea73a0f685df124dbe77c68d7be

--- device.mk
+++ device.mk
@@ -124,8 +124,12 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.device@3.6 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.6 \
+    vendor.qti.hardware.camera.device@1.0 \
     libgui_vendor \
     libstdc++.vendor \
     Snap
