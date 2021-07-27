9fd97bd94ba34ce815e6f27405d0f568724363e4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-27 16:43:31 +0530

    fixup! onclite: Lights: Add Support for Blink
    Signed-off-by: neonmicky <neonmicky30@gmail.com>

--- device.mk
+++ device.mk
@@ -264,7 +264,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.onclite
+    android.hardware.lights-service.onclite
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
