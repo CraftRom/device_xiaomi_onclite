219c2374d57874824af9d4df51a2d3cd0ae3f52f
Galitsky Dmytro <melles.android@gmail.com>
2021-08-03 22:01:37 +0300

    onclite: Rename proprietary-files.txt to proprietary-files-qc.txt

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,6 @@ function blob_fixup() {
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files-qc.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -30,7 +30,7 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 # Warning headers and guards
 write_headers
 
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
+write_makefiles "${MY_DIR}/proprietary-files-qc.txt" true
 
 # Finish
 write_footers
