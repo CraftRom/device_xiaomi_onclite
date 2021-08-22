34655cd41431c7a50769f7a4fec004a13dd07eae
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    msm8953-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- device.mk
+++ device.mk
@@ -450,6 +450,9 @@ PRODUCT_PACKAGES += \
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
+
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile	
 	
 # USB HAL
 PRODUCT_PACKAGES += \
