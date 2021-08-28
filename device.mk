2f885934a7a0b05eb305fd6d909f69410bca55e1
melles1991 <melles.android@gmail.com>
2021-08-28 15:43:36 +0300

    onclite: Build missing HIDLs 12-17 10:31:16.140   559   559 I hwservicemanager: getTransport: Cannot find entry android.hardware.vibrator@1.3::IVibrator/default in either framework or device manifest.

--- device.mk
+++ device.mk
@@ -157,7 +157,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.allocator@3.0-impl \
-    android.hardware.graphics.allocator@4.0-impl \    
+    android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
@@ -434,7 +434,8 @@ PRODUCT_PACKAGES += \
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service     \
+    android.hardware.vibrator@1.2
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
