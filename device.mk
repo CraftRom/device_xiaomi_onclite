37dfcff44522f05e7d0d2c189e0eabd269482afa
KostyaJRZ <kostyajrzaws@gmail.com>
2021-10-26 22:40:47 +0300

    onclite: Update Perf stack from LA.UM.8.6.2.r1-08400-89xx.0

--- device.mk
+++ device.mk
@@ -339,7 +339,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
+    vendor.qti.hardware.perf@2.0-service \
+    android.hardware.power@1.2 \
+    android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
